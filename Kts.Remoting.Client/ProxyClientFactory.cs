ab75960d77a028c286f5648491e44b5b0bb8f69d
Brannon King <countprimes@gmail.com>
2015-09-08 17:34:30 -0600

    one more file

---
+++ Kts.Remoting.Client/ProxyClientFactory.cs
@@ -0,0 +1,27 @@
+﻿using CommonSerializer;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Kts.Remoting.Client
+{
+	public class ProxyClientFactory
+	{
+		public ProxyClientFactory(ClientWebSocket socket, ICommonSerializer serializer, 
+			int messageBufferSize = 2000000, bool compressSentPackets = true)
+		{
+
+		}
+
+		public T GenerateProxy<T>() where T : class
+		{
+
+		}
+	}
+}
